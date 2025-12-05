1161a654436547274789b40ab6675eb33de66f53
Ahman04 <abokey2020@gmail.com>
2025-12-05 15:13:34 +0300

    Complete migrations and Alembic lab (initial + autogenerated migration)

---
+++ migrations/versions/1202da8e6b30_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 1202da8e6b30
+Revises: 
+Create Date: 2025-12-05 15:10:20.088300
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1202da8e6b30'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
